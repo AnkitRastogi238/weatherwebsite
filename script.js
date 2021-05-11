dd8385609e4bf34b5c6fc69cc427c5934f13ed7d
AnkitRastogi238 <77622890+AnkitRastogi238@users.noreply.github.com>
2021-05-12 01:58:01 +0530

    Add files via upload
    we have added some files to it

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#searchInput").on("keyup",function(e){
+        var cityname=e.target.value;
+        const APIKey="94515104f5547730ae5340fb72fed1bd";
+        //make request to server
+        $.ajax({
+            url:`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`
+
+        }).done(function(weatherdata){
+          console.log(weatherdata);
+          $("#profile").html(
+           `<div class=container>
+            <div class="row">
+                   <div class="card" style="width: 18rem;">
+                         <img class="card-img-top" src="http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png" alt="Card image cap">
+                         <div class="card-body">
+                             <h5 class="card-title">Weather:${weatherdata.weather[0].description}</h5>
+                             <p class="card-text">The Temperature at ${cityname} is =${weatherdata.main.temp} &#8451 and it feels like it is ${weatherdata.main.feels_like}</p>
+                             <p class="card-text">Country:${weatherdata.sys.country}</p>
+                             <a href="https://www.google.com/search?q=${cityname}" class="btn btn-primary">Learn more about this place</a>
+                         </div>
+                   </div>
+             </div> 
+             </div>`
+          ); 
+        })
+    });
+});
